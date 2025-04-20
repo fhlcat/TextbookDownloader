7fffe98e212a1da837a1caae2e48ca1529c522b8
fhlcat <fhlcat@outlook.com>
2025-04-20 20:16:12 +0800

    add logging

--- TextbookDownloaderCli/CliHandlers.fs
+++ TextbookDownloaderCli/CliHandlers.fs
@@ -2,23 +2,29 @@
 
 open System
 open System.IO
+open Serilog
 open TextbookDownloader.Book.TextbookInfo
 open TextbookDownloaderCli.GatherFolderInfo
 open TextbookDownloaderCli.CliTools.Folder
 open TextbookDownloaderCli.DownloadBooks
 
 let downloadFolderCommandHandler (authFilePath: string) (interval: int) (outputPath: string) =
-    let log (message: string) = Console.WriteLine message
-    let folders = gatherFoldersInfo log authFilePath
+    Log.Logger <- 
+        LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("log.txt")
+            .CreateLogger()
+    let folders = gatherFoldersInfo Log.Information authFilePath
     let selectedFolder = askForFolder folders
     let auth = File.ReadAllText authFilePath
 
     downloadBooks
         { interval = interval
           Events =
-            { Waiting = Some(fun _ -> ())
-              DownloadStarted = Some(fun book -> log $"Downloading {book.Name}...")
-              DownloadFinished = Some(fun book -> log $"Downloaded {book.Name} successfully.") }
+            { Waiting = Some(fun interval -> Log.Information $"Waiting {interval}ms...")
+              DownloadStarted = Some(fun book -> Log.Information $"Downloading {book.Name}...")
+              DownloadFinished = Some(fun book -> Log.Information $"Downloaded {book.Name} successfully.") }
           Auth = auth
           DirectoryInfo = DirectoryInfo(outputPath) }
         selectedFolder.Books
